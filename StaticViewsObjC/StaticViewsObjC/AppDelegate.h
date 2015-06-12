846cb34d75e0b96504ec41d1d1242dabc7c322f4
Michael Vilabrera <m.vilabrera@gmail.com>
2015-06-12 12:14:30 -0400

    initial commit

---
+++ StaticViewsObjC/StaticViewsObjC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  StaticViewsObjC
+//
+//  Created by Michael Vilabrera on 6/12/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ StaticViewsObjC/StaticViewsObjC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StaticViewsObjC
+//
+//  Created by Michael Vilabrera on 6/12/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
